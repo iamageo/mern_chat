40e4083010d4d1fab372b87aa796390ac07ce0e8
iamageo <geovani.af4@gmail.com>
2020-12-28 18:07:03 -0300

    mongodb connect and create controller folder

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+//Setup erros handlers
+const errorHandlers = require("./handlers/errorHandler")
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if(process.env.ENV === "DEVELOPMENT") {
+    app.use(errorHandlers.developmentErrors);
+
+} else {
+    app.use(errorHandlers.productionErrors)
+}
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+require("dotenv").config()
+
+const mongoose = require("mongoose")
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', (err)=> {
+    console.log("Erro ao conectar ao mongodb: ", err.message)
+});
+
+mongoose.connection.once('open', () => {
+    console.log("---- Conectado ao mongodb! ---")
+    console.log("------------------------------")
+});
+
 const app = require('./app')
 
 app.listen(8000, () => {
+    console.log("------------------------------")
     console.log('Servidor rodando na porta 8000')
+    console.log("------------------------------")
 })
