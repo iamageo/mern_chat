74e0c9939fb4ad13d2fa531db91ea25b344f3063
iamageo <geovani.af4@gmail.com>
2020-12-29 03:06:28 -0300

    create sal in env and registrer complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,27 @@
+const mongoose = require ('mongoose');
+const User = mongoose.model('User');
+const sha256 = require('js-sha256')
+
+exports.register = async(req, res) => {
+    const {name, email, password} = req.body;
+    const emailRegex = /[@gmail.com|@yahoo.com|@hotmail.com|@outlook.com|@live.com]$/
+
+    if(!emailRegex.test(email)) throw "Email is not supported from your domain";
+    if(password < 5) throw "Password must be alteadt 5 characters long."
+
+    const user = new User({
+        name, 
+        email, 
+        password: sha256(password + process.env.SALT)});
+
+    await user.save();
+
+    res.json({
+        message: "user ["  + name + "] registred successfull"
+    })
+}
+
+
 exports.login = async(req, res) => {
-    
+
 }
