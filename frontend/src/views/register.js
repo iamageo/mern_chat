8c079cc0df6ddce5c4e574aff78a89b9cc37bbf5
iamageo <geovani.af4@gmail.com>
2021-01-01 19:33:21 -0300

    delete files, create nem files and init code of login, register e dashboard

---
+++ frontend/src/views/register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const register = () => {
+  return (
+      <div>
+          register
+      </div>
+  )
+}
+
+export default register;
